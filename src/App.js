1eea758b948a02f5817499d27ca5d1a219d8c308
Enes KURU <enes793k@icloud.com>
2018-10-02 14:53:05 +0300

    high order component define

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+
+import Posts from './components/Posts';
 
 class App extends Component {
+
+  state = {
+    posts: []
+  };
+
+  componentWillMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts').then(res => res.data).then(posts => {
+      setTimeout(() => {
+        this.setState({posts});
+      }, 2000);
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-        </header>
+        <Posts {...this.state}/>
       </div>
     );
   }
---
+++ src/components/Posts.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import LoaderHOC from './LoaderHOC';
+
+class Posts extends Component {
+  render() {
+    return (
+      <div>
+        {
+          this.props.posts.map(post => <div key={post.id}>{post.title}</div>)
+        }
+      </div>
+    );
+  }
+}
+
+export default LoaderHOC(Posts);/*Bu şekilde loading i generic yapabiliyoruz*/
